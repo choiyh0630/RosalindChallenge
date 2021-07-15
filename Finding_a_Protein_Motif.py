5219e56720b149f75b558eaf605c7b756ba83838
Yoonhee Choi <choiyh@berkeley.edu>
2021-07-15 00:18:57 -0700

    7/15/2021 have to debug the query returning multiple results

--- Finding_a_Protein_Motif.py
+++ Finding_a_Protein_Motif.py
@@ -18,17 +18,19 @@
 # 4. If there are pattern matches, add the locations (the position in a string where the substring begins)
 # 5. Write to the output file the access IDs and the corresponding locations of N-glycosylation motifs for the protein sequence
 
+import re
 import urllib.parse
 import urllib.request
 
 url = 'https://www.uniprot.org/uploadlists/'
-s = 'B5ZC00 A2Z669 P07204_TRBM_HUMAN P20840_SAG1_YEAST'
+s = 'P00750_UROT_HUMAN P00740_FA9_HUMAN P17967 P07357_CO8A_HUMAN P01046_KNL1_BOVIN P10153_RNKD_HUMAN P01190_COLI_BOVIN Q82ZQ1 P55067_PGCN_RAT A1TJ10 P10643_CO7_HUMAN B9LIC8 P08198_CSG_HALHA P42098_ZP3_PIG Q50228'
 
 params = {
 'from': 'ACC+ID',
-'to': 'ACC+ID',
+'to': 'ACC',
 'format': 'fasta',
-'query': s
+'query': s,
+'include': 'no'
 }
 
 data = urllib.parse.urlencode(params)
@@ -37,3 +39,45 @@ req = urllib.request.Request(url, data)
 with urllib.request.urlopen(req) as f:
    response = f.read()
 print(response.decode('utf-8'))
+sequences = response.decode('utf-8').splitlines()
+ids = s.split(' ')
+print(ids)
+print(len(ids))
+count = 0
+seq_dic = {}
+# pattern = '\\|.*\\|'
+# for i in sequences:
+#     if i.startswith('>'):
+#         id = re.findall(pattern, i)[0][1:-1]
+#         print(id)
+
+for p in sequences:
+    if p.startswith('>'):
+        seq_dic[count] = ''
+        count += 1
+        print(count)
+    else:
+        seq_dic[count - 1] += p
+
+print(seq_dic)
+
+n_gly_pat = '(?=(N[^P][ST][^P]))'
+
+# loc_dic = {}
+# for key, seq in seq_dic.items():
+#     # print(key, ids[key])
+#     # print(seq)
+#     locations = [match.start() + 1 for match in re.finditer(n_gly_pat, seq_dic[key])]
+#     if locations:
+#         loc_dic[ids[key]] = locations
+#     else:
+#         continue
+
+#print(loc_dic)
+
+# output = open("Output_Finding_a_Protein_Motif.txt", 'w+')
+# for id, locs in loc_dic.items():
+#     output.write(id + "\n")
+#     output.write(' '.join(str(loc) for loc in locs) + "\n")
+# output.close()
+
