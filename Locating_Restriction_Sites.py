3c16757823f5c4b7eb07cf389d707164e3cd23dc
Yoonhee Choi <choiyh@berkeley.edu>
2021-07-22 21:31:32 -0700

    7/22/2021 problem solved

--- Locating_Restriction_Sites.py
+++ Locating_Restriction_Sites.py
@@ -11,7 +11,7 @@ seq_lst = f.read().splitlines()[1:]
 seq = ''.join(seq_lst)
 print(seq)
 
-# seq = 'TCAATGCATGCGGGTCTATATGCAT'
+# seq = 'TATATATATATA'
 # print(seq)
 
 
@@ -31,44 +31,19 @@ def reverse_complement(sequence):
 rev_c = reverse_complement(seq)
 print(rev_c)
 
-final = {}
+final = []
 for n in range(len(seq)):
-    for i in range(4,12):
+    for i in range(4,13): # reverse palindrome has to be length between 4 and 12
         sub = seq[n: n+i]
         rev_sub = reverse_complement(sub)
-        if len(sub) >= 4 and sub == rev_sub:
-            final[n + 1] = len(sub)
+        if n+i <= len(seq) and sub == rev_sub:
+            final.append((n+1, len(sub)))
+
 print(final)
 
-
-# dic = {}
-# for i in range(len(seq)):
-#     if i + 4 > len(seq):
-#         continue
-#     rev_range = rev_c[i+3:i+12]
-#     if seq[i] in rev_range:
-#         locs_rev = [loc for loc, n in enumerate(rev_range) if n == seq[i]]
-#         for l in locs_rev:
-#             l += 3
-#             temp_i = i
-#             pal = ''
-#             rev = rev_c[l+i]
-#             seq_v = seq[temp_i]
-#             while not l+i < i and rev_c[l+i] == seq[temp_i]:
-#                 if 4 <= len(pal) <= 12 and i not in dic:
-#                     dic[i] = ''
-#                 pal += seq[temp_i]
-#                 l -= 1
-#                 temp_i += 1
-#             if 4 <= len(pal) <= 12:
-#                     dic[i] = pal
-# print(dic)
-# final = {key+1: len(value) for key, value in dic.items()}
-# print(final)
-
 output = open('Output_Locating_Restriction_Sites.txt', 'w+')
-for key, value in final.items():
-    output.write(str(key) + ' ')
-    output.write(str(value) + '\n')
+for pair in final:
+    output.write(str(pair[0]) + '\t')
+    output.write(str(pair[1]) + '\n')
 output.close()
 
