b6a9fd6b3e61cb26edf34587cfb485067df754fc
Yoonhee Choi <choiyh@berkeley.edu>
2021-07-27 00:09:08 -0700

    7/27/2021 solving problem

--- Mortal_Fibonacci_Rabbits.py
+++ Mortal_Fibonacci_Rabbits.py
@@ -8,10 +8,32 @@
 m = 3
 
 def count_rabbits(n):
-    if n == 1 or n == 2:
-        total = 1
-    else:
-        total = count_rabbits(n - 1) + count_rabbits(n - 2) - count_rabbits(n - (m+1))
-    return total
+    rabbits = [0] * (n)
+    rabbits[0] = 1
+    rabbits[1] = 1
+    print(rabbits)
+    n -= 1
+    for i in range(2, n + 1):
+        if i + 1 > m:
+            rabbits[i] = rabbits[i - 1] + rabbits[i - 2] - rabbits[i-(m+1)]
+        else:
+            rabbits[i] = rabbits[i - 1] + rabbits[i - 2]
+
+    return rabbits
 
 print(count_rabbits(6))
+
+k = m
+n = 6
+f = [0] * (n + 1)
+f[0] = 1
+for i in range (2, n + 1):
+    print(f)
+    a =f[i-2]
+    b = f[i-1]
+    c = f[i-k-1]
+    x = i-k-1
+    f[i] = f[i-2] + f[i-1] - f[i - k - 1]
+
+print(f[n] + f[n-1])
+
