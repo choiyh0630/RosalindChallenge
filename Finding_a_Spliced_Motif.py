6bedab9cb1966bebd7e8bded10a6ff532fcdc224
Yoonhee Choi <choiyh@berkeley.edu>
2021-08-01 20:24:11 -0700

    8/1/2021 adding rosalind input

--- Finding_a_Spliced_Motif.py
+++ Finding_a_Spliced_Motif.py
@@ -4,8 +4,22 @@
 # Given: Two DNA Strings s and t (each of length at most 1 kbp) in FASTA format.
 # Return: One collection of indices of s in which the symbols of t appear as a subsequence of s.
 
-s = 'ACGTACGTGACG'
-t = 'GTA'
+f = open('rosalind_sseq.txt', 'r')
+next = f.readline()
+d = {}
+while next != '':
+    if next.startswith('>'):
+        tmp = next[1:].strip('\n')
+        d[tmp] = ''
+        next = f.readline().strip('\n')
+    d[tmp] += next
+    next = f.readline().strip('\n')
+print(d)
+
+s = list(d.values())[0]
+t = list(d.values())[1]
+# s = 'ACGTACGTGACG'
+# t = 'GTA'
 
 indices = []
 for i in range(len(t)):
@@ -16,7 +30,7 @@ for i in range(len(t)):
                 indices.append(id)
                 break
             elif i > 0:
-                if id < indices[i - 1] or abs(id - indices[i - 1] == 1):
+                if id < indices[i - 1] or abs(id - indices[i - 1] <= 1):
                     continue
                 indices.append(id)
                 break
