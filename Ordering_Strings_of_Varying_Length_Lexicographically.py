3d447c58bcb23cf3d60009af05e5a12fe3203bb8
Yoonhee Choi <choiyh@berkeley.edu>
2021-08-29 22:15:34 -0700

    8/29/2021 problem solved

---
+++ Ordering_Strings_of_Varying_Length_Lexicographically.py
@@ -0,0 +1,34 @@
+l = 'F M Z L C X P A K R'
+n = 4
+# l = 'D N A'
+# n = 3
+l = l.split(' ')
+
+def get_all_possible_seqs(n):
+    # if n == 0:
+    #     return []
+    if n == 1:
+        return l
+    else:
+        return get_next_set_of_seqs(get_all_possible_seqs(n-1), n-1)
+
+def get_next_set_of_seqs(seq_lst, n):
+    res = []
+    # if not seq_lst: # when n == 1, seq_lst is [] because
+    #     for i in l:
+    #         res.append(i)
+    #else:
+    for s in seq_lst:
+        res.append(s)
+        if len(s) == n:
+            for i in l:
+                res.append(s + i)
+    return res
+
+#print(get_all_possible_seqs(n))
+for i in get_all_possible_seqs(n):
+    print(i)
+
+output = open('Output_Ordering_Strings_of_Varying_Length_Lexicographically.txt', 'w')
+for i in get_all_possible_seqs(n):
+    output.write(str(i) + "\n")
