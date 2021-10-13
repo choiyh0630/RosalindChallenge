a1b853427445901a05619b1ca8e69deab58d2c51
Yoonhee Choi <choiyh@berkeley.edu>
2021-10-13 00:23:17 -0700

    10/13/2021 recursively solved; need to do dp way

--- Finding_a_Shared_Spliced_Motif.py
+++ Finding_a_Shared_Spliced_Motif.py
@@ -1,5 +1,23 @@
 from Bio import SeqIO
 
 dnas = SeqIO.parse("./Sample_Finding_a_Shared_Spliced_Motif.fa", "fasta")
+dna_lst = []
 for dna in dnas:
-    print(dna.seq)
+    dna_lst.append(str(dna.seq))
+
+s = dna_lst[0]
+t = dna_lst[1]
+
+print(dna_lst)
+
+def LCS(s1, s2):
+    if s1 == "" or s2 == "":
+        return 0
+    elif s1[-1] == s2[-1]:
+        return 1 + LCS(s1[:-1], s2[:-1])
+    else:
+        return max(LCS(s1[:-1], s2), LCS(s1, s2[:-1]))
+
+s = "abcde"
+t = "bce"
+print(LCS(s, t))
