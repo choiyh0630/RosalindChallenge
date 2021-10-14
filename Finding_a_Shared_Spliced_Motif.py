cebf989a03e07480d1fc41ccd6fed0cedf685782
Yoonhee Choi <choiyh@berkeley.edu>
2021-10-14 00:16:15 -0700

    10/14/2021 getting the correct answer with the example

--- Finding_a_Shared_Spliced_Motif.py
+++ Finding_a_Shared_Spliced_Motif.py
@@ -1,10 +1,12 @@
 from Bio import SeqIO
+import numpy as np
 
-dnas = SeqIO.parse("./Sample_Finding_a_Shared_Spliced_Motif.fa", "fasta")
+# dnas = SeqIO.parse("./Sample_Finding_a_Shared_Spliced_Motif.fa", "fasta")
+dnas = SeqIO.parse("./rosalind_lcsq.fa", "fasta")
 dna_lst = []
 for dna in dnas:
     dna_lst.append(str(dna.seq))
-
+dna_lst = sorted(dna_lst, key=len, reverse=True)
 s = dna_lst[0]
 t = dna_lst[1]
 
@@ -18,6 +20,40 @@ def LCS(s1, s2):
     else:
         return max(LCS(s1[:-1], s2), LCS(s1, s2[:-1]))
 
-s = "abcde"
-t = "bce"
-print(LCS(s, t))
+# s = "RDAA"
+# t = "DAGA"
+# print(LCS(s, t))
+
+dp_table = np.zeros((len(s) + 1, len(t) + 1))
+
+# to reflect the empty string in the dp table
+s = " " + s
+t = " " + t
+
+for i in range(1, len(s)):
+    for j in range(1, len(t)):
+        if s[i] == t[j]:
+            dp_table[i][j] = dp_table[i - 1][j - 1] + 1
+        else:
+            dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1])
+
+print(dp_table)
+
+i = len(s) - 1
+j = len(t) - 1
+lcs = ""
+# while dp_table[i][j] > 0:
+while i * j != 0:
+    curr = dp_table[i][j]
+    left = dp_table[i][j - 1]
+    up = dp_table[i - 1][j]
+    if left == up and dp_table[i-1][j-1] < dp_table[i][j]:
+        lcs += s[i]
+        i -= 1
+        j -= 1
+    elif left > up:
+        j -= 1
+    else:
+        i -= 1
+lcs = lcs[::-1]
+print(lcs)
