35d5d3157cac77db75f99f13d37cf839bde192a5
Yoonhee Choi <choiyh@berkeley.edu>
2021-10-14 20:39:51 -0700

    10/14/2021 problem solved

--- Finding_a_Shared_Spliced_Motif.py
+++ Finding_a_Shared_Spliced_Motif.py
@@ -12,6 +12,8 @@ t = dna_lst[1]
 
 print(dna_lst)
 
+
+# Recursion
 def LCS(s1, s2):
     if s1 == "" or s2 == "":
         return 0
@@ -20,10 +22,7 @@ def LCS(s1, s2):
     else:
         return max(LCS(s1[:-1], s2), LCS(s1, s2[:-1]))
 
-# s = "RDAA"
-# t = "DAGA"
-# print(LCS(s, t))
-
+# Dynamic Programming
 dp_table = np.zeros((len(s) + 1, len(t) + 1))
 
 # to reflect the empty string in the dp table
@@ -37,23 +36,33 @@ for i in range(1, len(s)):
         else:
             dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1])
 
-print(dp_table)
+print(dp_table[-1][-1])
 
+# Traverse the dp table to obtain the actual LCS string
 i = len(s) - 1
 j = len(t) - 1
 lcs = ""
-# while dp_table[i][j] > 0:
-while i * j != 0:
+while dp_table[i][j] > 0:
+# while i * j != 0:
     curr = dp_table[i][j]
     left = dp_table[i][j - 1]
     up = dp_table[i - 1][j]
-    if left == up and dp_table[i-1][j-1] < dp_table[i][j]:
+    if up == curr:
+        i -= 1
+    elif left == curr:
+        j -= 1
+    else:
         lcs += s[i]
         i -= 1
         j -= 1
-    elif left > up:
-        j -= 1
-    else:
-        i -= 1
+
+    # if left == up and dp_table[i-1][j-1] < dp_table[i][j]: --> this is wrong because you have to compare the current value to left or up, not just compare left and up
+    #     lcs += s[i]
+    #     i -= 1
+    #     j -= 1
+    # elif left > up:
+    #     j -= 1
+    # else:
+    #     i -= 1
 lcs = lcs[::-1]
 print(lcs)
