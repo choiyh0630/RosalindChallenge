92592168ddf7235ed56fad2845df7e2f1faf4c5a
Yoonhee Choi <choiyh@berkeley.edu>
2021-11-01 22:52:55 -0700

    11/1/2021 problem solved

--- Speeding_Up_Motif_Finding.py
+++ Speeding_Up_Motif_Finding.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 
-dna = SeqIO.read('./Sample_Speeding_Up_Motif_Finding.fa', 'fasta')
+# dna = SeqIO.read('./Sample_Speeding_Up_Motif_Finding.fa', 'fasta')
+dna = SeqIO.read('./rosalind_kmp.fa', 'fasta')
 s = dna.seq
 
 failure_arr = [0]
@@ -13,9 +14,24 @@ for i in range(1, len(s)):
     else:
         while j != 0:
             j = failure_arr[j - 1]
-
+            if s[i] == s[j]:
+                failure_arr.append(j + 1)
+                j += 1
+                break
         # when j = 0
-        failure_arr.append(0)
+        if j == 0:
+            if s[i] == s[j]:
+                failure_arr.append(j + 1)
+                j += 1
+            else:
+                failure_arr.append(0)
+
+print(failure_arr)
+t = ""
+for i in failure_arr:
+    t = t + " " + str(i)
+
+print(t)
 
 
 
